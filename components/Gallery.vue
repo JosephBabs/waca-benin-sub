106f25d2b583735def3577450e53cba17f7e3b8c
JosephBabs <bajos3d@gmail.com>
2023-12-06 01:14:35 +0100

    Waca-benin commit 5 December 2023(uigp)

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,20 +1,33 @@
 <template>
   <section class="gallery_two">
     <div class="container">
-      <div class="row masonary-layout" style="position: relative; height: 1011px;">
-        <div class="col-xl-4 col-lg-6 col-md-6 masonary-item" style="position: absolute; left: 0px; top: 0px;">
+      <div
+        class="row masonary-layout"
+        style="position: relative; height: 1011px;"
+      >
+        <div v-for="item in gallery"
+              :key="item.id"
+          class="col-xl-4 col-lg-6 col-md-6 masonary-item"
+          
+        >
           <div class="gallery_two_single">
             <div class="gallery_two_image">
-              <img src="/assets/images/gallery/gallery-2-img-1.jpg" alt="">
+              <img :src="item.image" 
+                :alt="item.title" />
               <div class="gallery_two_hover_box">
                 <div class="gallery_two_icon">
-                  <a class="img-popup" href="/assets/images/gallery/gallery-2-img-1.jpg"><span class="icon-plus-symbol"></span></a>
+                  <a
+                    class="img-popup"
+                    :href="item.image"
+                    ><span class="icon-plus-symbol"></span
+                  ></a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-xl-4 col-lg-6 col-md-6 masonary-item" style="position: absolute; left: 400px; top: 0px;">
+
+        <!-- <div class="col-xl-4 col-lg-6 col-md-6 masonary-item" style="position: absolute; left: 400px; top: 0px;">
           <div class="gallery_two_single">
             <div class="gallery_two_image">
               <img src="/assets/images/gallery/gallery-2-img-2.jpg" alt="">
@@ -85,23 +98,51 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
 </template>
 
 <script>
-    export default {
-      name: "Gallery",
-      mounted() {
-        new GLightbox({
-          selector: '.img-popup',
-        });
-      }
-    }
+export default {
+  name: "Gallery",
+  data() {
+    return {
+      gallery: [],
+      galleryV: [],
+      currentIndex: null,
+      lightboxVisible: false
+    };
+  },
+  methods: {
+    async getData() {
+      // const mediaLink = data.postImg.link;
+      // const apiLink = data.apiUrl.link;
+      const res = await fetch("https://app.cmabenin.bj/api/galleries");
+      const finalRes = await res.json();
+      this.gallery = finalRes.galleries;
+    },
+    async getData1() {
+      // const mediaLink = data.postImg.link;
+      // const apiLink = data.apiUrl.link;
+      const res1 = await fetch( "https://app.cmabenin.bj/api/video-galleries");
+      const finalRes1 = await res1.json();
+      this.galleryV = finalRes1.galleries;
+    },
+    
+    
+    
+  },
+  mounted() {
+    this.getData();
+    this.getData1();
+    
+    new GLightbox({
+      selector: ".img-popup"
+    });
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,10 +1,11 @@
 <template>
 
-  <section class="page-header" style="background-image: url(/assets/images/backgrounds/page-header-contact.jpg);">
+  <section class="page-header" style="background-image: url(/assets/images/backgrounds/page-header-contact.jpg); 
+background-color: rgba(0, 255, 0, 0.2);">
     <div class="container">
       <h2>{{ title }}</h2>
       <ul class="thm-breadcrumb list-unstyled">
-        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/">Accueil</nuxt-link></li>
         <li><span>{{ title }}</span></li>
       </ul>
     </div>
